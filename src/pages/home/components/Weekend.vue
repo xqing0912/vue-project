d2bba21ac4918aa2bb159e084eff2b6c78f26e94
xqing0912 <64568046+xqing0912@users.noreply.github.com>
2020-11-05 13:42:17 +0800

    add project

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="title">周末出游</div>
+        <ul>
+            <li class="item" border-bottom v-for="item of list" :key="item.id">
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl" />
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{item.title}}</p>
+                    <p class="item-desc">{{item.desc}}</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus">
+@import '~styles/weekendStyle/weekend-style.styl'
+</style>
